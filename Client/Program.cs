764992b1e6a93870bdceb4707f1ea914adc503f3
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2019-06-06 13:09:24 +0200

    Source hinzugefügt

---
+++ Client/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        const string SERVER_IP = "172.17.24.80";
+        const int SERVER_PORT = 9999;
+
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Bitte IP eingeben: ");
+            //string ip = Console.ReadLine();
+
+
+            //Console.WriteLine("Bitte Port eingeben: ");
+            //int port = Convert.ToInt32(Console.ReadLine());
+
+            //---create a TCPClient object at the IP and port no.---
+            Console.WriteLine("Connecting ...");
+            using (TcpClient client = new TcpClient(SERVER_IP, SERVER_PORT))
+            {
+                NetworkStream nwStream = client.GetStream();
+                Console.WriteLine("Connection established.");
+
+                do
+                {
+                    Console.Write(">> ");
+                    string textToSend = Console.ReadLine();
+                    if (textToSend[0] == 'x')
+                    {
+                        break;
+                    }
+                    byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+
+                    nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+                    byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                    int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                    string receivedText = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+                    Console.WriteLine("Received : " + receivedText);
+                } while (true);
+
+            }
+
+        }
+
+        private static void PrintServerResponse(string response)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"--> {response}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        const string SERVER_IP = "172.17.24.80";
+        const int SERVER_PORT = 9999;
+
+        static void Main(string[] args)
+        {
+            //---listen at the specified IP and port no.---
+            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+            TcpListener listener = new TcpListener(localAdd, SERVER_PORT);
+            Console.WriteLine("Listening...");
+            listener.Start();
+
+            //---incoming client connected---
+            TcpClient client = listener.AcceptTcpClient();
+
+            //---get the incoming data through a network stream---
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+
+
+            string dataReceived = "";
+            do
+            {
+                //---read incoming stream---
+                int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+                //---convert the data received into a string---
+                dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received : " + dataReceived);
+
+                //---write back the text to the client---
+                Console.WriteLine("Sending back : " + dataReceived);
+                nwStream.Write(buffer, 0, bytesRead);
+
+            } while (dataReceived == "x");
+
+            client.Close();
+            listener.Stop();
+            Console.ReadLine();
+        }
+    }
+}
